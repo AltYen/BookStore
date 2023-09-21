f378bbbb6c7dcba6a1ae96d09eb59c8aa3dcba4b
altanyenigun <altanyenigun@gmail.com>
2023-09-21 09:36:51 +0300

    adding basic custom logger middleware

---
+++ WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApi.Middlewares
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public CustomExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var watch = Stopwatch.StartNew(); // süre hesaplama için kullanılan timer mekanizması
+            string message = "[Request]  HTTP " + context.Request.Method + " - " + context.Request.Path;
+            Console.WriteLine(message);
+            await _next(context); // bir sonraki Middleware'i çağırma
+            watch.Stop(); // timeri durdur; | serviste ne kadar zaman harcandığını bu şekilde ölçüyoruz;
+            message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + "ms";
+            Console.WriteLine(message);
+        }
+    }
+
+    public static class CustomExceptionMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionMiddleware>();
+        }
+    }
+}
